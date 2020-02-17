3fd0c3fb5a7fa228740830b9ad4c15b35f297309
emanuelwille <42946195+emanuel92@users.noreply.github.com>
2020-02-17 21:45:56 +0100

    firebase 3

--- docs/index.js
+++ docs/index.js
@@ -1,41 +1,6 @@
-var firebase = require("firebase/app");
+import { initializeFirebase } from ".push.js";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDYvpd_lekcGqXEMl6gNN2mMtCng2-JOPg",
-  authDomain: "pwchat-9250c.firebaseapp.com",
-  databaseURL: "https://pwchat-9250c.firebaseio.com",
-  projectId: "pwchat-9250c",
-  storageBucket: "pwchat-9250c.appspot.com",
-  messagingSenderId: "846310791083",
-  appId: "1:846310791083:web:dbacdc221dfd13823da4ee",
-  measurementId: "G-Z2S3EZNSYN"
-};
-
-firebase.initializeFirebase = () => {
-  try {
-    firebase.initializeApp(firebaseConfig);
-    askForPermissionToReceiveNotifications();
-  } catch (err) {
-    if (!/already exists/.test(err.message)) {
-      console.error("Firebase initialization error", err.stack);
-    }
-  }
-};
-
-firebase.askForPermissionToReceiveNotifications = async () => {
-  try {
-    const messaging = firebase.messaging();
-
-    await messaging.requestPermission();
-    const token = await messaging.getToken();
-    console.log("user token: ", token);
-
-    return token;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
+initializeFirebase();
 const webSocketConnection =
   "wss://v1n46gbiah.execute-api.eu-west-1.amazonaws.com/dev";
 const turnServerIPAddress = "52.16.45.235";
