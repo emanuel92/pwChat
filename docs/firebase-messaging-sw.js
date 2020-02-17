038a8158676d910220d521b7f204b61f35d701d9
emanuelwille <42946195+emanuel92@users.noreply.github.com>
2020-02-17 22:14:04 +0100

    firebase 7

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.8.2/firebase-messaging.js");
 
 firebase.initializeApp({
   messagingSenderId: "YOUR-SENDER-ID"
